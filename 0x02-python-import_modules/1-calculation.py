5342826cef24a6bef563770757ddc43115129a61
scurry222 <scurry222@gmail.com>
2019-05-02 19:07:06 +0000

    adding check to see if script is a main file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-a = 10
-b = 5
-from calculator_1 import add
-print("{:d} + {:d} = {:d}".format((a), (b), (a + b)))
-from calculator_1 import sub
-print("{:d} + {:d} = {:d}".format((a), (b), (a - b)))
-from calculator_1 import mul
-print("{:d} + {:d} = {:d}".format((a), (b), (a * b)))
-from calculator_1 import div
-print("{:d} + {:d} = {:.0f}".format((a), (b), (a / b)))
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    from calculator_1 import add
+    print("{:d} + {:d} = {:d}".format((a), (b), (a + b)))
+    from calculator_1 import sub
+    print("{:d} + {:d} = {:d}".format((a), (b), (a - b)))
+    from calculator_1 import mul
+    print("{:d} + {:d} = {:d}".format((a), (b), (a * b)))
+    from calculator_1 import div
+    print("{:d} + {:d} = {:.0f}".format((a), (b), (a / b)))
