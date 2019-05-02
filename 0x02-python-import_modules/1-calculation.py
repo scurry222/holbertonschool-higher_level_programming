e30dbea67d6bce70090a0e30c51736391472cfdc
scurry222 <scurry222@gmail.com>
2019-05-02 19:17:58 +0000

    reformatting and adding task 4

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    from calculator_1 import add
+
     print("{:d} + {:d} = {:d}".format((a), (b), (a + b)))
-    from calculator_1 import sub
-    print("{:d} + {:d} = {:d}".format((a), (b), (a - b)))
-    from calculator_1 import mul
-    print("{:d} + {:d} = {:d}".format((a), (b), (a * b)))
-    from calculator_1 import div
-    print("{:d} + {:d} = {:.0f}".format((a), (b), (a / b)))
+    print("{:d} - {:d} = {:d}".format((a), (b), (a - b)))
+    print("{:d} * {:d} = {:d}".format((a), (b), (a * b)))
+    print("{:d} / {:d} = {:.0f}".format((a), (b), (a / b)))
