aa3a94de1ecd7272a72310e00e2ac3eef41baf51
scurry222 <scurry222@gmail.com>
2019-05-08 02:31:32 +0000

    (4) checking correct length (6) fixed compile error (12) removing extra path name

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     length = len(matrix[0])
     for i in range(len(matrix)):
-        for j in range(len(length)):
+        for j in range(len(matrix[0])):
             if j < length - 1:
                 print("{:d}".format(matrix[i][j]), end=" ")
             else:
