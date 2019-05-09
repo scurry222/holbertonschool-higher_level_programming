c9830ea4104d9fbefcec039a8c44212738dffac6
scurry222 <scurry222@gmail.com>
2019-05-09 22:07:43 +0000

    arithmetic to get correct number added to base_10

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,11 +5,11 @@ def roman_to_int(roman_string):
                'D': 500, 'M': 1000}
     if roman_string is None or type(roman_string) is not str:
         return 0
-    for index, char in enumerate(roman_string):
-        if char not in convert:
-            return 0
-        elif convert[char] >= convert[roman_string[index]]:
-            base_10 += convert[char]
+    for i in range(len(roman_string)):
+        if i > 0 and \
+             convert[roman_string[i]] > convert[roman_string[i - 1]]:
+             base_10 += convert[roman_string[i]] - 2 * \
+                        convert[roman_string[i - 1]]
         else:
-            base_10 -= convert[char]
+            base_10 += convert[roman_string[i]]
     return base_10
