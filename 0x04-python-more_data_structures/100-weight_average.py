256e9d6b8b7ea61fe132eff86081063b01b84858
scurry222 <scurry222@gmail.com>
2019-05-09 20:34:34 +0000

    fixed empty list edge case

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,7 +2,7 @@
 def weight_average(my_list=[]):
     n = sum([num[0] * num[1] for num in my_list])
     result = n / sum([num[1] for num in my_list])
-    if my_list is None:
-        return 0
-    else:
+    if my_list:
         return result
+    else:
+        return 0
