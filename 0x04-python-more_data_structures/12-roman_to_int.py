b40936b65227163d778233ec7a7385ef4ade0078
scurry222 <scurry222@gmail.com>
2019-05-09 19:58:46 +0000

    roman numeral to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    base_10 = 0
+    convert = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                'D': 500, 'M': 1000}
+    if roman_string is None:
+        return 0
+    for index, char in enumerate(roman_string):
+        if char not in convert:
+            return 0
+        elif convert[char] >= convert[roman_string[index]]:
+            base_10 += convert[char]
+        else:
+            base_10 -= convert[char]
+    return base_10
