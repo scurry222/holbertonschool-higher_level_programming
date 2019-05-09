9f40322c3108dd0ccf7bd1a72b333fe232abc4c4
scurry222 <scurry222@gmail.com>
2019-05-09 20:01:44 +0000

    check if roman_string is not a string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     base_10 = 0
     convert = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
                 'D': 500, 'M': 1000}
-    if roman_string is None:
+    if roman_string is None or type(roman_string) is not str:
         return 0
     for index, char in enumerate(roman_string):
         if char not in convert:
