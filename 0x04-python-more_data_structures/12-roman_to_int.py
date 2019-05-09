de0c91ab51a0a590b0f2e221c24acc1d3151c326
scurry222 <scurry222@gmail.com>
2019-05-09 22:10:46 +0000

    pep8 compliant

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,9 +6,8 @@ def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) is not str:
         return 0
     for i in range(len(roman_string)):
-        if i > 0 and \
-             convert[roman_string[i]] > convert[roman_string[i - 1]]:
-             base_10 += convert[roman_string[i]] - 2 * \
+        if i > 0 and convert[roman_string[i]] > convert[roman_string[i - 1]]:
+            base_10 += convert[roman_string[i]] - 2 * \
                         convert[roman_string[i - 1]]
         else:
             base_10 += convert[roman_string[i]]
