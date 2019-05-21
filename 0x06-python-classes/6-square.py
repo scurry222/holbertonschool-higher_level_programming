84612aaf78ad03b5844700b369b03a1fca2bcf95
scurry222 <scurry222@gmail.com>
2019-05-21 23:02:47 +0000

    adding position edge case

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -72,6 +72,7 @@ class Square:
 
         """
         if type(value) is not tuple or len(value) is not 2 or\
-           type(value[0]) is not int or value[0] < 0:
+           type(value[0]) is not int or value[0] < 0 or\
+           value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
