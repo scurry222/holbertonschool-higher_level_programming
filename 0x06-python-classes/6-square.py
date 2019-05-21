b0361b9b7b3db01d134ae38d6c7e8216d6334e29
scurry222 <scurry222@gmail.com>
2019-05-21 23:08:17 +0000

    reordering for readability

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -26,11 +26,38 @@ class Square:
         """
         return self.__size
 
+    @size.setter
+    def size(self, value):
+        """ Defines size of square object to change to value
+
+        Args:
+            size (obj:'int') size of the square
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        """ Only positive integers allowed for attribute size
+
+        """
+        self.__size = value
+
     @property
     def position(self):
         """ Defines position of square object
         """
         return self.__position
+
+    @position.setter
+    def position(self, value):
+        """ Sets position of value if its a tupe of 2 pos ints
+
+        """
+        if type(value) is not tuple or len(value) is not 2 or\
+           type(value[0]) is not int or value[0] < 0 or\
+           value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """ Defines area of square object
@@ -50,29 +77,4 @@ class Square:
             for i in range(self.__size):
                 print((" " * self.__position[0]) + ('#' * self.__size))
 
-    @size.setter
-    def size(self, value):
-        """ Defines size of square object to change to value
 
-        Args:
-            size (obj:'int') size of the square
-        """
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        """ Only positive integers allowed for attribute size
-
-        """
-        self.__size = value
-
-    @position.setter
-    def position(self, value):
-        """ Sets position of value if its a tupe of 2 pos ints
-
-        """
-        if type(value) is not tuple or len(value) is not 2 or\
-           type(value[0]) is not int or value[0] < 0 or\
-           value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
