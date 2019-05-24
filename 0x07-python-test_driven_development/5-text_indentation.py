09a95bc8215b24d6599c5534fae31060f4a15b71
scurry222 <scurry222@gmail.com>
2019-05-24 23:36:40 +0000

    new tests - rewritten text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,21 +3,10 @@
 def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
-
-    total = 0
-
-    for i in range(0, len(text)):
-        total += 1
-
-    space = False
-
-    for ch in text:
-        if ch is '.' or ch is '?' or ch is ':':
-            print("{}\n\n".format(ch), end="")
-            space = True
-            continue
-        if space and ch is ' ':
-            continue
-        if space and ch is not ' ':
-            space = False
-        print("{}".format(ch), end="")
+    text = text.replace(". ", ".\n\n").replace("? ", "?\n\n")\
+                        .replace(": ", ":\n\n")
+    strings = text.splitlines(keepends=True)
+    ret = ""
+    for words in strings:
+        ret += words.strip(" ")
+    print(ret, end="")
