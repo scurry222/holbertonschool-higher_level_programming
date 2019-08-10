0afaac6c0e9b5ad717e1ce9f8bc6a99536bb5300
scurry222 <scurry222@gmail.com>
2019-08-10 22:23:17 +0000

    fixed binary pattern search

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
                          user=argv[1], passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name = '{}'
+    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY '{}'
                 ORDER BY states.id ASC""".format(argv[4]))
     states = cur.fetchall()
     for row in states:
