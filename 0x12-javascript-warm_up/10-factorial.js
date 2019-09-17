4ff563bfdece5957376da74e36194d65d6678aa1
scurry222 <scurry222@gmail.com>
2019-09-17 23:03:21 +0000

    include zero

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,7 +4,7 @@ let fact = 1;
 if (process.argv[2] === undefined) {
   console.log(fact);
 } else {
-  for (let x = Number(process.argv[2]); x !== 1; x--) {
+  for (let x = Number(process.argv[2]); x !== 0; x--) {
     fact = fact * x;
   }
   console.log(fact);
