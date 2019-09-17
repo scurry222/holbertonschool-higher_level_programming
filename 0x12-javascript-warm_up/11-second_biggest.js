2af242f06854775acfa43de86a018c3b1262a307
scurry222 <scurry222@gmail.com>
2019-09-17 23:25:52 +0000

    semistandard format

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ if (process.argv.length <= 3) {
   console.log(arr);
 } else {
   arr = process.argv.slice(2);
-  arr.sort(function(a, b){return b-a});
+  arr.sort(function (a, b) { return b - a; });
   console.log(arr[1]);
 }
