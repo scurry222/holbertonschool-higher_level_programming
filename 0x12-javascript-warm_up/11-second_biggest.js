ca3b592d72565b945344ffaf273e261ba58e7c84
scurry222 <scurry222@gmail.com>
2019-09-17 23:24:25 +0000

    fix sort

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,6 @@ if (process.argv.length <= 3) {
   console.log(arr);
 } else {
   arr = process.argv.slice(2);
-  arr.sort();
-  arr.reverse();
+  arr.sort(function(a, b){return b-a});
   console.log(arr[1]);
 }
