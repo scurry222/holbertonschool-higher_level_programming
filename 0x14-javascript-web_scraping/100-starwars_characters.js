12ba0310096724a7c6b527e7acd6ce1cafbefefd
scurry222 <scurry222@gmail.com>
2019-09-24 21:52:17 +0000

    semistandard fix

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,12 +4,14 @@ request.get('http://swapi.co/api/films/' + process.argv[2], function (err, res, 
   if (res.statusCode === 200) {
     for (const character of JSON.parse(body).characters) {
       request.get(character, function (err1, res1, body1) {
-      if (res1.statusCode === 200) {
+        if (res1.statusCode === 200) {
           console.log(JSON.parse(body1).name);
         } else if (err1) {
           console.log(err1);
         }
       });
     }
+  } else if (err) {
+    console.log(err);
   }
 });
