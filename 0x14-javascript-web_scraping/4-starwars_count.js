29db8ac57f561ff70795f07ea81d832d6f2b274d
scurry222 <scurry222@gmail.com>
2019-09-24 20:45:36 +0000

    mandatory - different APIs

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+request.get('http://swapi.co/api/films/', function (err, res, body) {
+  let count = 0;
+  if (res.statusCode === 200) {
+    const films = (JSON.parse(body).results);
+    for (let i = 0; i < films.length; i++) {
+      for (let j = 0; j < films[i].characters.length; j++) {
+        if (films[i].characters[j].includes('18')) {
+          count += 1;
+        }
+      }
+    }
+    console.log(count);
+  } else if (err) {
+    console.log(err);
+  }
+});
