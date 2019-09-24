0e62e6e72df6708276ad9c8f795894568a8cf749
scurry222 <scurry222@gmail.com>
2019-09-24 22:28:23 +0000

    camelcase

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -8,7 +8,7 @@ request.get('http://swapi.co/api/films/' + process.argv[2], function (err, res, 
     for (const url of characters) {
       request.get(url, function (err1, res1, body1) {
         if (res1.statusCode === 200) {
-          saved_Data(url, JSON.parse(body1).name) 
+          savedData(url, JSON.parse(body1).name);
         } else if (err1) {
           console.log(err1);
         }
@@ -18,10 +18,10 @@ request.get('http://swapi.co/api/films/' + process.argv[2], function (err, res, 
     console.log(err);
   }
 });
-function saved_Data(url, name) {
+function savedData (url, name) {
   dict[url] = name;
   if (Object.entries(dict).length === characters.length) {
-    for (let url of characters) {
+    for (const url of characters) {
       console.log(dict[url]);
     }
   }
