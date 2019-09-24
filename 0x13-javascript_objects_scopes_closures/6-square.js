2baa9579794ca71b7fc9500ec12c74aa1cc8c9f2
scurry222 <scurry222@gmail.com>
2019-09-24 04:22:57 +0000

    reformat with repeat for rows, one function

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,53 +1,16 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w && w > 0 && h && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const rect = require ('./5-square');
 
-  print () {
-    let pX = 'X';
-    for (let y = 0; y < this.height; y++) {
-      for (let x = 1; x < this.width; x++) {
-        pX += 'X';
-      }
-      console.log(pX);
-      pX = 'X';
-    }
-  }
-
-  rotate () {
-    const temp = this.height;
-    this.height = this.width;
-    this.width = temp;
-  }
-
-  double () {
-    this.height = this.height * 2;
-    this.width = this.width * 2;
-  }
-}
-
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-    this.size = size;
-  }
-
+module.exports = class Square extends rect {
   charPrint (c) {
+    let pC;
     if (c) {
-      let pC = c;
-      for (let y = 0; y < this.size; y++) {
-        for (let x = 1; x < this.size; x++) {
-          pC += c;
-        }
-        console.log(pC);
-        pC = c;
-      }
+      pC = c;
     } else {
-      this.print();
+      pC = 'X';
+    }
+    for (let y = 0; y < this.height; y++) {
+      console.log(pC.repeat(this.width));
     }
   }
 };
