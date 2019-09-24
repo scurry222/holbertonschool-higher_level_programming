78c04294f6d4d16499d25099cb26f3d83d6458d7
scurry222 <scurry222@gmail.com>
2019-09-24 21:11:27 +0000

    stable search

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,7 +6,7 @@ request.get('http://swapi.co/api/films/', function (err, res, body) {
     const films = (JSON.parse(body).results);
     for (let i = 0; i < films.length; i++) {
       for (let j = 0; j < films[i].characters.length; j++) {
-        if (films[i].characters[j].includes('18')) {
+        if (films[i].characters[j].includes("/18/")) {
           count += 1;
         }
       }
